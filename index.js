56a05e8f70705539f7a289ad3680ad8627d7e5a4
H3RMIT <shubhamhalder0@gmail.com>
2022-06-28 20:54:31 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const PORT  = process.env.PORT || 4000
+
+app.get('/',(req,res) => {
+    res.status(200).send('Hello There!')
+})
+
+app.listen(PORT,() => {
+    console.log(`Listening on port ${PORT}`);
+})
