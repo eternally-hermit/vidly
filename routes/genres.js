3e4e3d31095313e56027a7b74aaa587cbf219e01
H3RMIT <shubhamhalder0@gmail.com>
2022-07-06 11:50:19 +0530

    Code Restructured Again

--- routes/genres.js
+++ routes/genres.js
@@ -1,24 +1,10 @@
+const {Genre,validate} = require('../models/genre');
 const mongoose = require('mongoose')
-const Joi = require('joi');
 const express = require('express');
 const router = express.Router();
 
 
-const Genre = mongoose.model('Genre',new mongoose.Schema({
-  name:{
-    type:String,
-    required:true,
-    minlength:5,
-    maxlength:50
-  }
-}));
 
-
-// const genres = [
-//   { id: 1, name: 'Action' },  
-//   { id: 2, name: 'Horror' },  
-//   { id: 3, name: 'Romance' },  
-// ];
 
 router.get('/', async (req, res) => {
   const genres = await Genre.find().sort('name');
@@ -26,7 +12,7 @@ router.get('/', async (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  const { error } = validateGenre(req.body); 
+  const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.message);
 
   let genre = new Genre({name: req.body.name});
@@ -36,7 +22,7 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
 
-  const { error } = validateGenre(req.body); 
+  const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.message);
 
   const genre = await Genre.findByIdAndUpdate(req.params.id,{name:req.body.name},{
@@ -63,11 +49,6 @@ router.get('/:id', async (req, res) => {
   res.send(genre);
 });
 
-function validateGenre(genre) {
-  const schema = Joi.object({
-    name:Joi.string().min(3).required()
-})
-return schema.validate(genre)
-}
+
 
 module.exports = router;
