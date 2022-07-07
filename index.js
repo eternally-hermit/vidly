b858ed160e923e21321f9286b8591979b64bac84
eternally-hermit <shubhamhalder0@gmail.com>
2022-07-07 19:36:32 +0530

    Worked on modeling relationships

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
-const mongoose = require('mongoose')
-const Joi = require('joi');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers')
-const express = require('express');
+const mongoose = require("mongoose");
+//const Fawn = require("fawn");
+const Joi = require("joi");
+const genres = require("./routes/genres");
+const customers = require("./routes/customers");
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
+const express = require("express");
 const app = express();
 
 // This is a commment
 
-mongoose.connect('mongodb://localhost/vidly')
-    .then(() => console.log('Connected to MongoDB..!'))
-    .catch((err) => console.error(`Couldn't connect to MongoDB..!`))
+mongoose
+  .connect("mongodb://localhost/vidly")
+  .then(() => {
+    console.log("Connected to MongoDB..!");
+  })
+  .catch((err) => console.error(`Couldn't connect to MongoDB..!`));
 
 app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
+app.use("/api/genres", genres);
+app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- models/genre.js
+++ models/genre.js
@@ -1,21 +1,24 @@
-const mongoose = require('mongoose');
-const Joi = require('joi');
+const mongoose = require("mongoose");
+const Joi = require("joi");
 
-const Genre = mongoose.model('Genre',new mongoose.Schema({
-    name:{
-      type:String,
-      required:true,
-      minlength:5,
-      maxlength:50
-    }
-  }));
+const genreSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50,
+  },
+});
 
-  function validateGenre(genre) {
-    const schema = Joi.object({
-      name:Joi.string().min(3).required()
-  })
-  return schema.validate(genre)
-  }
-  
-  module.exports.Genre = Genre;
-  module.exports.validate = validateGenre;
+const Genre = mongoose.model("Genre", genreSchema);
+
+function validateGenre(genre) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(genre);
+}
+
+module.exports.Genre = Genre;
+module.exports.validate = validateGenre;
+module.exports.genreSchema = genreSchema;
